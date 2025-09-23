1358af4e66324ebb79cc5e98ff5f99babd342845
Yuuki <153100258+im-yuuki@users.noreply.github.com>
2025-09-23 17:52:38 +0700

    feat: libgdx framework for the game client (#2)
    * build(deps): use libgdx framework for the game client
    
    * refactor: make the client project structure simpler

---
+++ client/src/main/java/io/exterminator3618/client/Game.java
@@ -0,0 +1,20 @@
+package io.exterminator3618.client;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Game extends ApplicationAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(Game.class);
+
+	@Override
+    public void render() {
+        log.trace("Rendering a frame");
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+}
