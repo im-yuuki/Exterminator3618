5b1b6c7c21f828f372cbc1c7442edadd0ab4598a
Yuuki <me@june8th.eu.org>
2025-09-28 01:40:34 +0700

    refactor: remove unnecessary constants

--- client/src/main/java/io/exterminator3618/client/Constants.java
+++ client/src/main/java/io/exterminator3618/client/Constants.java
@@ -5,45 +5,15 @@ package io.exterminator3618.client;
  * used throughout the Exterminator3618 client application.
  */
 public class Constants {
-    // Window configuration
+
     /**
-     * Default window width in pixels.
+     * Window configuration.
      */
     public static final int WINDOW_WIDTH = 1280;
-    /**
-     * Default window height in pixels.
-     */
     public static final int WINDOW_HEIGHT = 720;
-    /**
-     * Window title.
-     */
     public static final String WINDOW_TITLE = "Exterminator3618";
-    /**
-     * Whether to enable VSync.
-     */
-    public static final boolean VSYNC_ENABLED = true;
-    /**
-     * FPS when the window is unfocused (idle).
-     */
-    public static final int IDLE_FPS = 10;
-    /**
-     * 16x16 window icon path.
-     */
-    public static final String ICON_16_PATH = "icons/logo.png";
-    /**
-     * 32x32 window icon path.
-     */
-    public static final String ICON_32_PATH = "icons/logo32.png";
-    /**
-     * 64x64 window icon path.
-     */
-    public static final String ICON_64_PATH = "icons/logo64.png";
-    /**
-     * 128x128 window icon path.
-     */
-    public static final String ICON_128_PATH = "icons/logo128.png";
 
-    // Ball defaults
+
     /**
      * Default ball width in pixels.
      */
